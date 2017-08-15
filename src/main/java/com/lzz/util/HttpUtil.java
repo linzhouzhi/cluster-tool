643ecf339b6ca4d71439e223168a8bdef6e0f57e
linzhouzhi <490664646@qq.com>
2017-08-15 23:08:19 +0800

    rest ful api

--- src/main/java/com/lzz/util/HttpUtil.java
+++ src/main/java/com/lzz/util/HttpUtil.java
@@ -1,5 +1,7 @@
 package com.lzz.util;
 
+import net.sf.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,36 +15,30 @@ import java.util.Map;
  * Created by lzz on 2017/8/14.
  */
 public class HttpUtil {
+
+    public static JSONObject jsonGet(String url){
+        String strRes = sendGet( url );
+        String res =  "{'result':" + strRes + "}";
+        JSONObject result = JSONObject.fromObject( res );
+        return result;
+    }
+
     /**
      * 向指定URL发送GET方法的请求
-     *
-     * @param url
-     *            发送请求的URL
-     * @param param
-     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
-     * @return URL 所代表远程资源的响应结果
      */
-    public static String sendGet(String url, String param) {
+    public static String sendGet(String url) {
         String result = "";
         BufferedReader in = null;
         try {
-            String urlNameString = url + "?" + param;
+            String urlNameString = url;
             URL realUrl = new URL(urlNameString);
             // 打开和URL之间的连接
             URLConnection connection = realUrl.openConnection();
             // 设置通用的请求属性
             connection.setRequestProperty("accept", "*/*");
             connection.setRequestProperty("connection", "Keep-Alive");
-            connection.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             // 建立实际的连接
             connection.connect();
-            // 获取所有响应头字段
-            Map<String, List<String>> map = connection.getHeaderFields();
-            // 遍历所有的响应头字段
-            for (String key : map.keySet()) {
-                System.out.println(key + "--->" + map.get(key));
-            }
             // 定义 BufferedReader输入流来读取URL的响应
             in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
@@ -86,8 +82,6 @@ public class HttpUtil {
             // 设置通用的请求属性
             conn.setRequestProperty("accept", "*/*");
             conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
             // 发送POST请求必须设置如下两行
             conn.setDoOutput(true);
             conn.setDoInput(true);
@@ -97,6 +91,14 @@ public class HttpUtil {
             out.print(param);
             // flush输出流的缓冲
             out.flush();
+
+            // 获取所有响应头字段
+            Map<String, List<String>> map = conn.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+
             // 定义BufferedReader输入流来读取URL的响应
             in = new BufferedReader(
                     new InputStreamReader(conn.getInputStream()));
--- src/test/java/TesqHttpUtil.java
+++ src/test/java/TesqHttpUtil.java
@@ -7,14 +7,51 @@ import org.junit.Test;
  */
 public class TesqHttpUtil {
     @Test
-    public void test1(){
+    public void registry_list(){
+        JSONObject result = HttpUtil.jsonGet("http://192.168.31.140:5000/v2/_catalog");
+        System.out.println( result );
+    }
 
-        String s = HttpUtil.sendGet("http://192.168.31.140:2376/containers/json", "");
-        System.out.println( s );
-        s = "{'result':" + s + "}";
-        JSONObject result = JSONObject.fromObject( s );
+    @Test
+    public void container_list(){
+        JSONObject result = HttpUtil.jsonGet("http://192.168.31.140:2376/containers/json");
         System.out.println( result );
 
+    }
+
+    @Test
+    public void container_detail(){
+        JSONObject result = HttpUtil.jsonGet("http://192.168.31.140:2376/containers/054c234e5782/json");
+        System.out.println( result );
+    }
+
+    @Test
+    public void container_restart(){
+        String s = HttpUtil.sendPost("http://192.168.31.140:2376/containers/054c234e5782/restart","t=1");
+        System.out.println( s );
+    }
+
+    @Test
+    public void container_stop(){
+        String s = HttpUtil.sendPost("http://192.168.31.140:2376/containers/054c234e5782/stop","t=1");
+        System.out.println( s );
+    }
+
+    @Test
+    public void container_start(){
+        String s = HttpUtil.sendPost("http://192.168.31.140:2376/containers/054c234e5782/start","t=1");
+        System.out.println( s );
+    }
+
+    @Test
+    public void container_rename(){
+        String s = HttpUtil.sendPost("http://192.168.31.140:2376/containers/054c234e5782/rename","name=registry2.5");
+        System.out.println( s );
+    }
+    @Test
+    public void image_list(){
+        JSONObject result = HttpUtil.jsonGet("http://192.168.31.140:2376/images/json");
+        System.out.println( result );
     }
 
 }
